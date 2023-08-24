510f6a0c49bdf130bf48b6e9f28a0b20a51c71da
Aman Jha <99260113+Aman-jha-8@users.noreply.github.com>
2023-08-24 20:04:34 +0530

    Added solution - LeetHub

--- Multiply two strings - GFG/multiply-two-strings.java
+++ Multiply two strings - GFG/multiply-two-strings.java
@@ -25,8 +25,8 @@ class Solution {
     public String multiplyStrings(String s1, String s2) {
         BigInteger b1 = new BigInteger(s1);
         BigInteger b2 = new BigInteger(s2);
-        BigInteger b= b1.multiply(b2);
-        String s = b.toString();
+        BigInteger b3 = b1.multiply(b2);
+        String s = String.valueOf(b3);
         return s;
     }
 }
