9ae45b8edfd71f764a3a0f085dde4c37a9bf2ac0
Aman Jha <99260113+Aman-jha-8@users.noreply.github.com>
2023-09-03 10:19:33 +0530

    Added solution - LeetHub

---
+++ Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.cpp
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Return True if the given trees are isomotphic. Else return False.
+    bool isIsomorphic(Node *root1, Node *root2) {
+        stack<Node*> stack1;
+        stack<Node*> stack2;
+        stack<Node*> stack3;
+        stack1.push(root1);
+        stack2.push(root2);
+        stack3.push(root2);
+        
+        while (!stack1.empty() && !stack2.empty()) {
+            Node *curr = stack1.top();
+            stack1.pop();
+            Node *curr2 = stack2.top();
+            stack2.pop();
+            Node *curr3 = stack3.top();
+            stack3.pop();
+            
+            if (curr->data != curr2->data && curr->data != curr3->data) {
+                return false;
+            }
+            
+            if (curr->left) {
+                stack1.push(curr->left);
+            }
+            if (curr->right) {
+                stack1.push(curr->right);
+            }
+            if (curr2->right) {
+                stack2.push(curr2->right);
+            }
+            if (curr2->left) {
+                stack2.push(curr2->left);
+            }
+            if (curr3->left) {
+                stack3.push(curr3->left);
+            }
+            if (curr3->right) {
+                stack3.push(curr3->right);
+            }
+        }
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s1,s2;
+		getline(cin,s1);
+		getline(cin,s2);
+		Node* root1 = buildTree(s1);
+		Node* root2 = buildTree(s2);
+		Solution obj;
+		if(obj.isIsomorphic(root1,root2))
+		    cout<<"Yes"<<endl;
+		else
+		    cout<<"No"<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
