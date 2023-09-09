eeb44fb88f791a73202e0a125e09606d2f21eaee
Aman Jha <99260113+Aman-jha-8@users.noreply.github.com>
2023-09-09 11:15:14 +0530

    Added solution - LeetHub

---
+++ Kth largest element in BST - GFG/kth-largest-element-in-bst.java
@@ -0,0 +1,140 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str)
+    {
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+  
+	public static void main (String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            int t=Integer.parseInt(br.readLine());
+            
+            while(t > 0){
+                String s = br.readLine();
+                Node root = buildTree(s);
+                
+                int k=Integer.parseInt(br.readLine());
+                
+                Solution g = new Solution();
+                System.out.println (g.kthLargest(root,k));
+                t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution {
+    int count = 0; // variable to keep track of the count
+    int result = -1; // variable to store the result
+	void kthLargestUtil(Node root, int k) {
+        
+        if (root == null || count >= k) // base case: if root is null or count is greater than or equal to k
+            return; 
+        kthLargestUtil(root.right, k); // recursively call the function on the right child
+        
+        count++; // increment the count
+
+        if (count == k) { // if count is equal to k
+            result = root.data; // update the result with the data value of current root
+            return; 
+        }
+        kthLargestUtil(root.left, k); // recursively call the function on the left child
+    } 
+  
+    int kthLargest(Node root, int k) { 
+        kthLargestUtil(root, k); // call the utility function to find the kth largest element
+        return result; // return the result
+    } 
+}
