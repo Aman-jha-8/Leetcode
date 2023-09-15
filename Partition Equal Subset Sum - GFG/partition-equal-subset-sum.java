70e9b5c1c1bf8b416113605303910e6c94d5e34c
Aman Jha <99260113+Aman-jha-8@users.noreply.github.com>
2023-09-15 19:44:08 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
@@ -31,25 +31,28 @@ class GFG{
 // User function Template for Java
 
 class Solution{
-    static int solve(int sum,int []arr,int i,int [][]dp){
-        if(sum==0)return 1;
-        if(i>=arr.length||sum<0)return 0;
-        if(dp[i][sum]!=-1)return dp[i][sum];
-        if(solve(sum-arr[i],arr,i+1,dp)==1||solve(sum,arr,i+1,dp)==1)return dp[i][sum]=1;
-        return dp[i][sum]= 0;
-    }
     static int equalPartition(int N, int arr[])
     {
         // code here
-        int sum=0;
         
+        int sum=0;
         for(int i=0;i<N;i++)
-        sum+=arr[i];
-        if(sum%2!=0)return 0;
-        sum/=2;
-        int [][]dp= new int [N+1][sum+1];
+            sum+=arr[i];
+        if(sum%2!=0) return 0;
+        sum=sum/2;
+        int[][] dp = new int[N+1][sum+1];
         for(int i=0;i<=N;i++)
-        Arrays.fill(dp[i],-1);
-        return solve(sum,arr,0,dp);
+            Arrays.fill(dp[i],-1);
+        return solve(arr,0,dp,sum);
+        
+    }
+    
+    public static int solve(int[] arr,int i,int[][] dp,int sum){
+        if(sum==0) return 1;
+        if(i >= arr.length || sum < 0) return 0;
+        if(dp[i][sum]!=-1)
+            return dp[i][sum];
+        if(solve(arr,i+1,dp,sum-arr[i])==1 || solve(arr,i+1,dp,sum) == 1) return dp[i][sum]=1;
+        return dp[i][sum] = 0;
     }
 }
