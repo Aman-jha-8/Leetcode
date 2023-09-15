1e4a961342939acba5f840b5aec4b9a8b6c4a696
Aman Jha <99260113+Aman-jha-8@users.noreply.github.com>
2023-09-15 18:54:31 +0530

    Added solution - LeetHub

---
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            int x = ob.equalPartition(N, arr);
+            if(x == 1)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static int solve(int sum,int []arr,int i,int [][]dp){
+        if(sum==0)return 1;
+        if(i>=arr.length||sum<0)return 0;
+        if(dp[i][sum]!=-1)return dp[i][sum];
+        if(solve(sum-arr[i],arr,i+1,dp)==1||solve(sum,arr,i+1,dp)==1)return dp[i][sum]=1;
+        return dp[i][sum]= 0;
+    }
+    static int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum=0;
+        
+        for(int i=0;i<N;i++)
+        sum+=arr[i];
+        if(sum%2!=0)return 0;
+        sum/=2;
+        int [][]dp= new int [N+1][sum+1];
+        for(int i=0;i<=N;i++)
+        Arrays.fill(dp[i],-1);
+        return solve(sum,arr,0,dp);
+    }
+}
